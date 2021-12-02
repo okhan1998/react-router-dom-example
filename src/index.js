779714e6c586d88950d40774a46d5aaf5c666f28
okhan1998 <whwlgns111@gmail.com>
2021-12-02 15:11:00 +0900

    add Nested Routing and useParams

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import {
   Route,
   Outlet, 
   Link,
-  NavLink
+  NavLink,
+  useParams
 } from "react-router-dom";
 
 function Home(){
@@ -30,12 +31,51 @@ function Contact(){
   )
 }
 
+let contents = [
+  {id:1, title:'HTML', description:'HTML is ...'},
+  {id:2, title:'JS', description:'JS is ...'},
+  {id:3, title:'REACT', description:'REACT is ...'}
+]
+
+function Topic() {
+  let params = useParams();
+  let topic_id = params.topic_id;
+  let selected_topic = {
+    title:'Sorry',
+    description: 'Not Found'
+  }
+  for(let i = 0; i<contents.length; i++){
+    if(contents[i].id === +topic_id){
+      selected_topic = contents[i];
+      break;
+    }
+  }
+  console.log(params);
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+    </div>
+  )
+
+}
+
+
 function Topics(){
+  let lis = [];
+  for(let i = 0; i<contents.length; i++){
+    lis.push(<li><NavLink to={'/topics/'+contents[i].id}>{contents[i].title}</NavLink></li>)
+  }
   return(
     <div>
       <h2>Topics</h2>
-      Topics...
+      <ul>
+        {lis}
+      </ul>
+      <Outlet/>
+      
     </div>
+    
   )
 }
 
@@ -51,8 +91,10 @@ function App(){
       </ul>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/topics" element={<Contact />} />
-        <Route path="/contact" element={<Topics />} />
+        <Route path="/topics" element={<Topics />} >
+          <Route path="/topics/:topic_id" element={<Topic />}/>
+        </Route>
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   )
