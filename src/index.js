f7234024aa9983b229ea90d3d4bfc80d2064258a
okhan1998 <whwlgns111@gmail.com>
2021-12-02 13:26:23 +0900

    Add basic Route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function Home(){
+  return(
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  )
+}
+
+function Contact(){
+  return(
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  )
+}
+
+function Topics(){
+  return(
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  )
+}
+
+
+function App(){
+  return (
+    <div>
+      <h1>React Router DOM example</h1>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/topics">Topics</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/topics" element={<Contact />} />
+        <Route path="/contact" element={<Topics />} />
+      </Routes>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter><App /></BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
